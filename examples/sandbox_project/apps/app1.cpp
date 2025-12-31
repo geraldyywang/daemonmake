cdbbb02439a2e723a02bd05ef934ea26004d24b5
Gerald Wang <gerald.wang@mail.utoronto.ca>
2025-12-31 13:55:15 -0500

    update sandbox_project with daemonmake generation

--- examples/sandbox_project/apps/app1.cpp
+++ examples/sandbox_project/apps/app1.cpp
@@ -1,5 +1,8 @@
 #include "sandbox_project/top.hpp"
 
+#include <iostream>
+
 int main() {
-    return a();
+    std::cout << "Expected 3, Got: " << a() << std::endl; 
+    return a();  // expect 3
 }
